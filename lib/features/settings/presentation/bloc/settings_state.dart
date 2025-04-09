6b69043cf9249265d8c65b8b4e564104163f244a
Kubanych Asanov <kuba.asanov4@gmail.com>
2025-04-09 12:02:39 +0600

    Migrated freezed from v2 to v3

--- lib/features/settings/presentation/bloc/settings_state.dart
+++ lib/features/settings/presentation/bloc/settings_state.dart
@@ -1,7 +1,7 @@
 part of 'settings_bloc.dart';
 
 @freezed
-class SettingsState with _$SettingsState {
+abstract class SettingsState with _$SettingsState {
   const factory SettingsState({
     ThemeMode? themeMode,
     Locale? locale,
--- lib/features/sign_in_feature/presentation/bloc/sign_in_state.dart
+++ lib/features/sign_in_feature/presentation/bloc/sign_in_state.dart
@@ -1,15 +1,14 @@
 part of 'sign_in_bloc.dart';
 
 @freezed
-class SignInState with _$SignInState {
+abstract class SignInState with _$SignInState {
   const factory SignInState({
     required SignInStatus status,
     SomeData? someData,
   }) = _SignInState;
 
-  factory SignInState.initial() 
-    => const SignInState(status: SignInStatus.initial);
+  factory SignInState.initial() =>
+      const SignInState(status: SignInStatus.initial);
 
   const SignInState._();
 }
-
